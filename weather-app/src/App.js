fdec388a96fd28bf35af404234d1384af528f3c2
alwinabraham <alwinabraham098@gmail.com>
2023-03-11 21:00:34 +0530

    changing to React part 2nd

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   
   const [city,setCity] = useState('Republic of India')
+  const [check,setCheck] = useState()
   const [name,setName] = useState()
   const [icon,setIcon] = useState()
   const [description,setDescription] = useState()
@@ -16,7 +17,7 @@ function App() {
   useEffect(() => {
     fetchWeather(city)
   })
-
+  
   function fetchWeather(city){
       fetch(
           `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
@@ -39,33 +40,38 @@ function App() {
     setDescription(descriptiondata)
     setWind(winddata)
   }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    setCity(check)
+  }
   
   return (
-    <body>
-        <div className="card">
-            <div className="search" id="search">
-                <input type="text" onClick={(e)=>setCity(e.target.value)} className="search-bar" placeholder="Search" />
-                <button id="button">
-                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1.5em"
-                        width="1.5em" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z">
-                        </path>
-                    </svg>
-                </button>
+    <div className="card">
+        <form onSubmit={handleSubmit}>
+          <div className="search" id="search">
+              <input type="text" onChange={(e)=>setCheck(e.target.value)} className="search-bar" placeholder="Search" />
+              <button type='submit' id="button">
+                  <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1.5em"
+                      width="1.5em" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                          d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z">
+                      </path>
+                  </svg>
+              </button>
+          </div>
+        </form>
+        <div>
+            <h2 className="city">Weather in {name}</h2>
+            <h1 className="temp">{temp}°C</h1>
+            <div className="flex">
+                <img src={`http://openweathermap.org/img/wn/${icon}.png`} alt=""/>
+                <div className="description">{description}</div>
             </div>
-            <div>
-                <h2 className="city">Weather in {name}</h2>
-                <h1 className="temp">{temp}°C</h1>
-                <div className="flex">
-                    <img src={`http://openweathermap.org/img/wn/${icon}.png`}/>
-                    <div className="description">{description}</div>
-                </div>
-                <div className="humidity">Humidity: {humidity}%</div>
-                <div className="wind">Wind Speed: {wind}km/h</div>
-            </div>
+            <div className="humidity">Humidity: {humidity}%</div>
+            <div className="wind">Wind Speed: {wind}km/h</div>
         </div>
-    </body>
+    </div>
   );
 }
 
